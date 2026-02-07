bf0668ba8fd85ca46b44c681c0c1609486b1daf1
qaizzzthh <202410715117@mhs.ubharajaya.ac.id>
2026-02-08 00:36:27 +0700

    Phase-0-Preparation-qaizzzthh-NumPattern

---
+++ quiz/Quiz-Fcking-Number-Pattern/Soal_1.js
@@ -0,0 +1,83 @@
+/*
+================
+MISSING NUMBER
+================
+description: Sebuah fungsi untuk mencari angka yang hilang berdasarkan pola dari board atau
+papan yang tersedia. Fungsi akan mengembalikan nilai sebuah array yang berisi
+angka-angka yang hilang
+
+examples:
+INPUT = 
+[
+  [ 7 ,' ', 5 ],
+  [' ', 8 , 9 ]
+  [ 1 ,' ',' '] 
+]
+
+ASUMSI PADA PAPAN SUDAH TERDAPAT RANGE TERBESAR DAN TERKECIL YAITU 1 DAN 9 SEHINGGA
+OUTPUT:
+[ 2, 3, 4, 6]
+
+PADA MASING-MASING TEST CASE SUDAH TERDAPAT RANGE TERBESAR DAN TERKECIL
+*/
+
+function missingNum(arr) {
+  //code here
+  let terkecil = Infinity;
+  let terbesar = -Infinity;
+  //Looping buat nyari terbesar terkecil
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] !== " ") {
+        if (arr[i][j] < terkecil) {
+          terkecil = arr[i][j];
+        } else if (arr[i][j] > terbesar) {
+          terbesar = arr[i][j];
+        }
+      }
+    }
+  }
+
+  let angka_yang_ada = [];
+  //Looping buat tau ada angka apa aja
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] !== " ") {
+        angka_yang_ada.push(arr[i][j])
+      }
+    }
+  }
+
+  let hasil = []
+  //Loop angka yang ga ada
+  for (let i = terkecil; i <= terbesar; i++) {
+    if (!angka_yang_ada.includes(i)) {
+      hasil.push(i);
+    }
+  }
+  
+  return hasil;
+}
+
+
+console.log(missingNum([
+  [3, ' ', 5],
+  [1, ' ', 7],
+  [9, ' ', ' ']
+])) // [ 2, 4, 6, 8 ]
+console.log(missingNum([
+  [2, ' '],
+  [' ', 5]
+])) // [ 3, 4 ]
+console.log(missingNum([
+  [11, ' ', 13],
+  [17, ' ', 19],
+  [' ', 16, ' ']
+])) // [ 12, 14, 15, 18 ]
+console.log(missingNum([
+  [3, ' ', 5, 15],
+  [1, ' ', 7, 13],
+  [9, ' ', ' ', 12],
+  [' ', 16, ' ', ' ']
+])) // [ 2, 4, 6, 8, 10, 11, 14 ]
+console.log(missingNum([])) // []
