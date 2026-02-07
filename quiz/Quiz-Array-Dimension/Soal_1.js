140d5a1ff86d4db662f77204de213fb824d510f3
qaizzzthh <202410715117@mhs.ubharajaya.ac.id>
2026-02-08 00:34:40 +0700

    Phase-0-Preparation-qaizzzthh-ArrayDimension

---
+++ quiz/Quiz-Array-Dimension/Soal_1.js
@@ -0,0 +1,45 @@
+/*
+=====================
+DOUBLE REVERSE ARRAY
+=====================
+
+[INSTRUCTION]
+Terdapat function doubleReverse yang menerima parameter berupa array of string, 
+function ini akan memutar elemen array, string yang berada di dalam elemen array juga akan dibalik atau diputar jika panjang string GENAP
+
+[EXAMPLE]
+input: ['rabu', 'cinta', 'benci', 'masuk', 'nikmat']
+proses: memutar isi array, dan memutar elemen array yang panjang katanya genap
+output: [ 'tamkin', 'masuk', 'benci', 'cinta', 'ubar' ]
+
+[RULES]
+- Function bawaan javascript yang boleh digunakan hanyalah .push dan .length
+- DILARANG menggunakan .reverse()
+*/
+
+function doubleReverse(arr) {
+    //code here
+    let hasil = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let balik = "";
+        if (arr[i].length % 2 === 0) {
+            for (let j = arr[i].length - 1; j >= 0; j--) {
+                balik += arr[i][j];
+            }
+            arr[i] = balik;
+        }
+        hasil.push(arr[i]);
+    }
+
+    return hasil;
+}
+
+console.log(doubleReverse(['rabu', 'cinta', 'benci', 'masuk', 'nikmat']));
+// [ 'tamkin', 'masuk', 'benci', 'cinta', 'ubar' ]
+console.log(doubleReverse(['aku', 'sayang', 'kamu']));
+// [ 'umak', 'gnayas', 'aku' ]
+console.log(doubleReverse(['pelakor', 'perusak', 'rumah', 'tangga']));
+//[ 'anggnat', 'rumah', 'perusak', 'pelakor' ]
+console.log(doubleReverse([]))
+// invalid input parameter
---
+++ quiz/Quiz-Array-Dimension/Soal_4.js
@@ -0,0 +1,49 @@
+/*
+==================================
+Array Mastery: Average Length Word
+==================================
+Nama:________
+[INSTRUKSI]
+Disediakan sebuah kalimat. Function averageLengthWord akan menerima satu parameter berupa string
+yang berisikan kalimat tersebut, dan akan mendapatkan rata-rata jumlah huruf dari setiap kata,
+kemudian mengembalikan nilai berupa array of string yang berisikan kata mana saja yang jumlahnya
+sama dengan rata-rata jumlah kata.
+
+NOTE:
+ - Spasi tidak dihitung
+ - Jika hasil rata-rata adalah desimal maka dibulatkan
+
+[CONTOH]
+input (kalimat): Do you want to become a great coder.
+rata-rata panjang kata dalam kalimat: 3
+output: ['you']
+
+input (kalimat): You dont know what you have until you lose it!.
+rata-rata panjang kata dalam kalimat: 4
+output: ['dont', 'know', 'what', 'have', 'lose']
+*/
+
+function averageLengthWord(words) {
+  //code here
+  let hasil = [];
+  const pisah = words.split(" ");
+
+  let jumlah = 0;
+  for (let i = 0; i < pisah.length; i++) {
+    jumlah += pisah[i].length;
+  }
+
+  let rata_rata = Math.round(jumlah / pisah.length);
+
+  for (let i = 0; i < pisah.length; i++) {
+    if (pisah[i].length === rata_rata) {
+      hasil.push(pisah[i]);
+    }
+  }
+
+  return hasil;
+}
+
+console.log(averageLengthWord('Do you want to become a great coder ?')); // ['you']
+console.log(averageLengthWord('You dont know what you have until you lose it!')); // [ 'dont','know','what','have','lose']
+console.log(averageLengthWord('I am diligent')); // []
